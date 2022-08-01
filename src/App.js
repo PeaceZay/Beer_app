382ede0fba1722b4d71b7eb4e7aafb0191d2ceb6
PeaceZay <zay.smith007@gmail.com>
2022-08-01 18:11:33 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import BeerCard from './BeerCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrayOfBeer: []
+    }//,
+    // can add more things here!
+  };
+
+  componentDidMount(){
+    fetch("https://api.punkapi.com/v2/beers")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      this.setState({
+        arrayOfBeer: data
+      })
+    })
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ol>{this.state.arrayOfBeer.map((beer, index) => {
+          return (
+            <BeerCard key={index} 
+            name ={beer.name} 
+            image_url = {beer.image_url} 
+            first_brewed = {beer.first_brewed} 
+            tagline = {beer.tagline} 
+            abv = {beer.abv} 
+            description = {beer.description}
+            ></BeerCard>
+          )
+        })}</ol>
+      </div>
+    );
+
+  }
 }
-
 export default App;
---
+++ src/BeerCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function BeerCard(props) {
+    return (
+        <li style = {{listStyle: "none"}}>
+            <h3>{props.name} <span style = {{color: "blue"}}>{props.first_brewed}</span></h3>
+            <h4>{props.tagline}</h4>
+            <img style = {{height: "150px" }} src = {props.image_url}></img>
+            <p style = {{textAlign: "right"}}><span style = {{color: "Red"}}>{props.abv}</span> {props.description}</p>
+        </li>
+    )
+}
+
+export default BeerCard

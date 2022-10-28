8b249408a7e4c14fa6eb11790d17333c9a3b8dd2
PeaceZay <zay.smith007@gmail.com>
2022-10-27 19:11:34 -0500

    completed styling

--- src/BeerCard.js
+++ src/BeerCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function BeerCard(props) {
     return (
-        <li style = {{listStyle: "none"}}>
-            <h3>{props.name} <span style = {{color: "blue"}}>{props.first_brewed}</span></h3>
+        <li style= {{listStyle: "none"}}>
+            <h3>{props.name} <span style= {{color: "blue"}}>{props.first_brewed}</span></h3>
             <h4>{props.tagline}</h4>
-            <img style = {{height: "150px" }} src = {props.image_url}></img>
-            <p style = {{textAlign: "right"}}><span style = {{color: "Red"}}>{props.abv}</span> {props.description}</p>
+            <img style= {{height: "150px" }} src = {props.image_url}></img>
+            <p style= {{textAlign: "left"}}><span style= {{color: "Red"}}>{props.abv}</span> {props.description}</p>
         </li>
     )
 }
